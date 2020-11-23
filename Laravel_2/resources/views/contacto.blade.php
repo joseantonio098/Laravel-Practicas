041bd058b613ea9452c5400c8d1777b334cc725f
joseantonio098 <joseantoniovalen1@gmail.com>
2020-11-23 06:06:48 -0500

    Laravel 23/11/20

---
+++ Laravel_2/resources/views/contacto.blade.php
@@ -0,0 +1,18 @@
+<!-- Queremos heredar de la plantilla.blade.php -->
+@extends("layouts.plantilla")
+
+<!-- Invocamos a la sección cabecera -->
+@section("cabecera")
+    <h1> Página de contacto </h1>
+@endsection
+
+
+@section("contenidoCentral")
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti 
+    dignissimos eum laborum beatae fuga repudiandae necessitatibus veniam 
+    tempore minima itaque.</p>
+@endsection
+
+@section("footer")
+
+@endsection
---
+++ Laravel_2/resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plantilla</title>
+    <style>
+        .cabecera{
+            background-color: black;
+            color: white;
+            text-align: center;
+            padding: 2px 0px;
+        }
+        .contenidoCentral{
+            background-color: green;
+            margin: 100px 0px;
+            text-align: center;
+            color: white;
+            font-weight: bold;
+            padding: 2px 0px;
+        }
+        .footer{
+            background-color: chocolate;
+            text-align: center;
+            color: white;
+            font-weight: bold;
+            padding: 2px 0px;
+        }
+    </style>
+</head>
+<body>
+   <!-- Creando la estructura de las páginas (Plantilla) --> 
+
+    <section class="cabecera">
+
+        @yield("cabecera")  <!-- Sección 1 -->
+
+    </section>
+        
+    <section class="contenidoCentral">
+
+        @yield("contenidoCentral")  <!-- Sección 2 -->
+
+    </section>
+
+    <section class="footer">
+
+        @yield("footer")    <!-- Sección 3 -->
+        <p>Derechos Reservador <b>joseantoniovalen1@gmail.com</b></p>
+
+    </section>
+</body>
+</html>
