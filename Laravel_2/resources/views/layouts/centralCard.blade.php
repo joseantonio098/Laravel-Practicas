8f7250154c0853f61b2516a0fa2930e810140dd9
joseantonio098 <joseantoniovalen1@gmail.com>
2023-07-11 08:06:31 -0500

    Laravel 11/07/23

--- Laravel_2/resources/views/layouts/centralCard.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="card">
-  <div class="card-body d-flex flex-column align-items-center">
-    <h5 class="card-title">Contenido Central</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-dark">Go somewhere</a>
-  </div>
-</div>
